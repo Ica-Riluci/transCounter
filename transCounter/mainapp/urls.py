80f069685e3038012e86c04f29e265fbd55b69e1
Ica Riluci <ica.riluci@gmail.com>
2019-01-20 20:35:08 +0800

    > login backend logic

--- transCounter/mainapp/urls.py
+++ transCounter/mainapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login')
+    path('login/', views.loginp, name='login'),
+    path('login_check/', views.login_check, name='login_check')
 ]
--- transCounter/mainapp/views.py
+++ transCounter/mainapp/views.py
@@ -1,21 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .forms import *
 
 # Create your views here.
+global nouser
+global wrongpw
 
+nouser = False
+wrongpw = False
+
+@login_required
 def index(request):
+    # logout(request)
     return HttpResponse('Hello, world. You\'re at the mainapp\'s index.')
 
-def login(request):
+def login_check(request):
+    global nouser
+    global wrongpw
     if request.method == 'GET':
-        f = LoginForm()
-        context= {
-            'form' : f
-        }
-        return render(request, 'mainapp/login.html', context)
+        return HttpResponseRedirect('/main/login')
     else:
+        nouser = False
+        wrongpw = False
         f = LoginForm(request.POST)
         if f.is_valid():
-            return HttpResponseRedirect('/main/')
+            try:
+                usr = User.objects.get(username=request.POST['username'])
+                lusr = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+                if lusr is not None:
+                    login(request, lusr)
+                    return HttpResponseRedirect('/main/')
+                else:
+                    wrongpw = True
+                    return HttpResponseRedirect('/main/login/')
+            except User.DoesNotExist:
+                nouser = True
+                return HttpResponseRedirect('/main/login/')
+
+def loginp(request):
+    global nouser
+    global wrongpw
+    f = LoginForm()
+    context= {
+        'form' : f,
+        'nouser' : nouser,
+        'wrongpw' : wrongpw
+    }
+    return render(request, 'mainapp/login.html', context)
